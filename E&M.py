0c81be73e42a5357132a4e4bd452e11bab4406e5
Rbot427 <rbot427@gmail.com>
2016-11-07 13:13:21 -0500

    Added a bunch of stuff

--- E&M.py
+++ E&M.py
@@ -20,13 +20,13 @@ def e_field(x, y, pp):
 	dx = x - pp[PP_X]
 	dy = y - pp[PP_Y]
 	scalar = e_mag/v2_mag((dx, dy))
-	return v2_norm((dx*scalar, dy*scalar))
+	return (dx*scalar, dy*scalar)
 
 def e_field_total(x, y, pp_all):
 	tot = (0, 0)
 	for i in pp_all:
 		tot = v2_sum(tot, e_field(x, y, i))
-	return tot
+	return v2_norm(tot)
 
 def line_for_v2(v, x, y):
 	#Error checking
@@ -54,25 +54,40 @@ def test_all_pp_lines(canvas, pp_all, target):
 	while theta < 2*math.pi:
 		x = pp[PP_X] + rad*math.cos(theta)
 		y = pp[PP_Y] + rad*math.sin(theta)
+		draw_full_field_line(canvas, pp_all, x, y)
+		theta += math.pi/8
+
+def draw_full_field_line(canvas, pp_all, x, y):
+	c = 0
+	while c < 100:
 		e = e_field_total(x, y, pp_all)
 		l = line_for_v2(e, x, y)
 		canvas.create_line(l[0], l[1], l[2], l[3], fill='blue')
-		theta += math.pi/8
+		x = x + e[0]
+		y = y + e[1]
+		c += 1
 
 def draw_pp(canvas, pp):
 	rad = pp[RADIUS]
-	canvas.create_oval(pp[PP_X]-rad/2, pp[PP_Y]-rad/2, pp[PP_X]+rad/2, pp[PP_Y]+rad/2, fill="red", width=2)
+	f = "red"
+	if pp[CHARGE] < 0: f = "blue"
+	canvas.create_oval(pp[PP_X]-rad/2, pp[PP_Y]-rad/2, pp[PP_X]+rad/2, pp[PP_Y]+rad/2, fill=f, width=2)
 
 all_pp = []
 p1 = createPP(200/3.0, 50, 10, 0.000000001)
 all_pp.append(p1)
-p2 = createPP(400/3.0, 50, 10, 0.000000001)
+p2 = createPP(400/3.0, 50, 10, -0.000000001)
 all_pp.append(p2)
+#p3 = createPP(150, 75, 10, -0.000000001)
+#all_pp.append(p3)
 root = Tk()
 w = Canvas(root, width=200, height=100)
 draw_pp(w, p1)
 draw_pp(w, p2)
+#draw_pp(w, p3)
 w.pack()
+#draw_full_field_line(w, all_pp, 75, 59)
 test_all_pp_lines(w, all_pp, 0)
-test_all_pp_lines(w, all_pp, 1)
+#test_all_pp_lines(w, all_pp, 1)
+#test_all_pp_lines(w, all_pp, 2)
 root.mainloop()
--- vectors.py
+++ vectors.py
@@ -1,6 +1,6 @@
 import math
 
-NORM_MAG = 30
+NORM_MAG = 2
 
 def v2_mag(v):
 	#Error checking
@@ -13,3 +13,11 @@ def v2_norm(v):
 
 def v2_sum(v1, v2):
 	return (v1[0] + v2[0], v1[1] + v2[1])
+
+def v2_scalar_multiple(s, v):
+	return (v[0]*s, v[1]*s)
+
+def test_v2_norm():
+	v = (0.1, 0.1)
+	print v2_norm(v)
+
