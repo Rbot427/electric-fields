ddc880adbc43b079cb424fdf631b611d9ad87aa4
Rbot427 <rbot427@gmail.com>
2016-11-07 11:28:31 -0500

    Split into separate file

---
+++ vectors.py
@@ -0,0 +1,15 @@
+import math
+
+NORM_MAG = 30
+
+def v2_mag(v):
+	#Error checking
+	return math.sqrt(v[0]**2 + v[1]**2)
+
+def v2_norm(v):
+	#Error checking
+	mag = float(v2_mag(v))
+	return (NORM_MAG*v[0]/mag, NORM_MAG*v[1]/mag)
+
+def v2_sum(v1, v2):
+	return (v1[0] + v2[0], v1[1] + v2[1])
